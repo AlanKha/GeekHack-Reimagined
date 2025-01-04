6972beda557697ad1cff2758ca6dedbe67a103f4
Alan Khalili <khalilialan@gmail.com>
2025-01-04 12:28:46 -0800

    Implement dynamic post retrieval and update PostPage layout

--- src/app/[categoryName]/[postName]/page.tsx
+++ src/app/[categoryName]/[postName]/page.tsx
@@ -1,3 +1,5 @@
+import { getPost } from "~/server/db/queries";
+
 export default async function PostPage({
   params,
 }: {
@@ -5,10 +7,13 @@ export default async function PostPage({
 }) {
   // eslint-disable-next-line @typescript-eslint/await-thenable
   const { categoryName, postName } = await params;
+  const post = await getPost(categoryName, postName);
+
   return (
     <div>
-      <h1>Category: {categoryName}</h1>
-      <h1>Post: {postName}</h1>
+      <h1>Category: {post.categoryId}</h1>
+      <h1>Post: {post.title}</h1>
+      <p>Content: {post.content}</p>
     </div>
   );
 }
